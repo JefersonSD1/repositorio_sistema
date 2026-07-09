f2a78f12309694a54647240c31db4f33b91da58b
Jeferson Estiven Cañas Guevara <6051227@incb.edu.sv>
2026-07-09 08:15:50 -0600

    Agregada funcionalidad DELETE dinámica y confirmación JavaScript

--- inventario.php
+++ inventario.php
@@ -65,6 +65,7 @@ text-decoration: none; border-radius: 5px;">+ Nuevo Producto</a>
 <th>Categoría</th>
 <th>Stock</th>
 <th>Precio Unitario</th>
+<th>Acciones</th>
 </tr>
 </thead>
 <tbody>
@@ -84,10 +85,20 @@ $claseStock = ($fila['stock'] < 10) ? 'stock-bajo' : '';
 <td> <?php echo $fila['nombre_categoria']; ?> </td>
 <td class="<?php echo $claseStock; ?>"> <?php echo $fila['stock']; ?> unds. </td>
 <td> $<?php echo number_format($fila['precio'], 2); ?> </td>
+<td>
+<a href="eliminar_producto.php?id=<?php echo $fila['id']; ?>"
+class="btn-eliminar"
+onclick="return confirm('¿Estás absolutamente seguro de eliminar el producto: <?php
+echo $fila['nombre_producto']; ?>?');">
+🗑️ Eliminar
+</a>
+</td>
+</tr>
+<?php } // Fin del bucle while ?>
 </tr>
 <?php
 } // Fin del bucle while
-} else {
+else {
 ?>
 <!-- Si la tabla está vacía, mostramos este mensaje -->
 <tr>
